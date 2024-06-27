3397f2d624dfdccaa32f624ded8b13edbfc0aa1f
tungnt155 <nguyenthanhtung99hb@gmail.com>
2024-06-27 11:27:22 +0700

    find clock hand by contours

--- test find contours.py
+++ test find contours.py
@@ -24,7 +24,7 @@ cv2.circle(temp, center=(centre[0], centre[1]), radius=5, color=(0, 255, 0), thi
 shortlist = []
 for i in contours:
     x, y, w, h = cv2.boundingRect(i)
-    if (centre[0] < (x+w) and centre[0] > x) and (centre[1]>y and centre[1]<y+h):
+    if ((x + w) > centre[0] > x) and (y < centre[1] < y + h):
         shortlist.append(i)
 # cv2.drawContours(temp, shortlist, -1, (255, 0, 0), 2)
 
@@ -40,7 +40,7 @@ for i in shortlist:
 cv2.drawContours(temp, handContour, -1, (255, 0, 0), 2)
 cv2.imshow('hand contour', temp)
 
-# Getting and Clustering Hull Points
+# # Getting and Clustering Hull Points
 hull = cv2.convexHull(handContour)
 # for i in range(0, len(hull)):
 #     cv2.circle(temp, center=(hull[i][0][0], hull[i][0][1]), radius=5, color=(100, 100, 100), thickness=-1)
@@ -48,19 +48,6 @@ hull = cv2.convexHull(handContour)
 cv2.drawContours(temp, [hull], -1, (0, 0, 255), 2)
 cv2.imshow('Convex Hull', temp)
 
-# hull_sorted = sorted(hull, key=lambda x: x[0][0]) # sắp xếp đỉnh convex tăng dần theo tọa độ x
-# print(hull)
-# threshold = 19  # Ngưỡng khoảng cách giữa các đỉnh
-# unique_vertices = []
-# for i in range(len(hull_sorted)):
-#     if i == 0 or abs(hull_sorted[i][0][0] - unique_vertices[-1][0]) > threshold:
-#         unique_vertices.append(hull_sorted[i][0])
-#
-# # In tọa độ các đỉnh duy nhất
-# for vertex in unique_vertices:
-#     print(vertex)
-#     cv2.circle(temp, center=(vertex[0], vertex[1]), radius=5, color=(100, 100, 100), thickness=-1)
-# cv2.imshow('Unique vertex', temp)
 
 def simplify_convex_hull(hull, epsilon):
     # Chuyển đổi danh sách các điểm convex hull sang định dạng numpy array
@@ -70,13 +57,42 @@ def simplify_convex_hull(hull, epsilon):
     # Chuyển đổi danh sách các điểm đơn giản hóa trở lại định dạng danh sách các điểm
     simplified_hull = simplified_points.squeeze().tolist()
     return simplified_hull
+
+
+def get_angle(image, hand, center):
+    x_h = hand[0]
+    y_h = hand[1]
+    x_c = center[0]
+    y_c = center[1]
+    cv2.arrowedLine(image, (x_c, y_c), (x_h, y_h), (0, 0, 255), 2)
+    dx = x_h - x_c
+    dy = y_h - y_c
+    angle = 0
+    if dx > 0 > dy:
+        angle = np.pi/2 - np.arctan(abs(dy / dx))
+    elif dx > 0 and dy > 0:
+        angle = np.pi/2 + np.arctan(abs(dy / dx))
+    elif dx < 0 < dy:
+        angle = 3*np.pi/2 - np.arctan(abs(dy / dx))
+    elif dx < 0 and dy < 0:
+        angle = 3 * np.pi / 2 + np.arctan(abs(dy / dx))
+    return angle*180/np.pi
+
+
 print(simplify_convex_hull(hull, 8))
 unique_vertices = simplify_convex_hull(hull, 8)
 unique_vertices = np.array(unique_vertices, dtype=np.int32)
-for vertex in unique_vertices:
-    print(vertex)
-    cv2.circle(temp, center=(vertex[0], vertex[1]), radius=5, color=(100, 100, 100), thickness=-1)
-cv2.imshow('Unique vertex', temp)
+sorted_vertices = sorted(unique_vertices, key=lambda vertex: np.sqrt((centre[0] - vertex[0])**2 + (centre[1] - vertex[1])**2), reverse=True)
+print(sorted_vertices)
+second_hand = sorted_vertices[0]
+minute_hand = sorted_vertices[1]
+hour_hand = sorted_vertices[2]
+
+second_value = int(get_angle(temp, second_hand, centre)*60 / 360)
+minute_value = int(get_angle(temp, minute_hand, centre)*60 / 360)
+hour_value = int(get_angle(temp, hour_hand, centre)*12 / 360)
+
+print(f"time = {hour_value} : {minute_value} : {second_value}")
 cv2.imshow('Unique vertex', temp)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
